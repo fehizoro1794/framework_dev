c23b1a862d24160f1fe69b039a5a47d2d3c9f9ff
fehizoro1794 <fehizoro.rabearisolo@gmail.com>
2023-03-07 14:32:55 +0100

    a

--- Sprint2/src/Mapping.java
+++ Sprint2/src/Mapping.java
@@ -2,17 +2,20 @@ package etu1794.framework;
 
 public class Mapping {
     String className;
-
-    public String test_method() {
-        return "test";
-    }
-
+    String methode;
     public String getClassName() {
         return className;
     }
-
     public void setClassName(String className) {
         this.className = className;
     }
+    public String getMethode() {
+        return methode;
+    }
+    public void setMethode(String methode) {
+        this.methode = methode;
+    }
+
+    
     
 }
