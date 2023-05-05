456867232201bf22614fe70d4e87f07aef00911e
fehizoro1794 <fehizoro.rabearisolo@gmail.com>
2023-05-05 10:19:15 +0100

    sprint7

--- framework/src/ModelView.java
+++ framework/src/ModelView.java
@@ -3,12 +3,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package etu1794.framework;
+
+import java.util.HashMap;
+
 /**
  *
  * @author rango;
  */
 public class ModelView {
     private String vue;
+    private HashMap<String, Object> data=new HashMap<>();
+
+    public void addIdem(String key, Object value){
+        this.getData().put(key, value);
+    }
 
     public ModelView(String vue) {
         this.setVue(vue);
@@ -24,6 +32,14 @@ public class ModelView {
 
     public void setVue(String vue) {
         this.vue = vue;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
     }
     
 }
